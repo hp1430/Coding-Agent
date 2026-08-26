0ce41768e5c17411fa74da46a66ea90c6881cd0b
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-08-26 17:41:07 +0530

    added provider client

---
+++ src/client.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+@dataclass(frozen=True)
+class Provider:
+    name: str
+    env_var: str
+    base_url: str
+    model:str
+
+PROVIDERS = [
+    Provider(
+        name = "Groq",
+        env_var = "GROQ_API_KEY",
+        base_url = "https://api.groq.com/openai/v1",
+        model = "openai/gpt-oss-20b"
+    )
+]
+
+def select_provider() -> Provider:
+    for provider in PROVIDERS:
+        if os.getenv(provider.env_var):
+            return provider
+
+    raise RuntimeError("No provider found")
+
+def get_client_and_model() -> tuple[OpenAI, str, Provider]:
+    provider = select_provider()
+    api_key = os.getenv(provider.env_var)
+
+    client = OpenAI(
+        api_key=api_key,
+        base_url=provider.base_url
+    )
+
+    return client, provider.model, provider
